b1cd89d47aea4d4694bdb7f11e009a3d941b0bc3
AbhishekSontakke02 <abhisontakke3930@gmail.com>
2024-09-02 11:39:55 +0530

    Refactor: Validate Userdtls to LambdaFuncationl Interface using Predicate Functional Interface

--- src/main/java/com/UserRegistration/UserRegistration.java
+++ src/main/java/com/UserRegistration/UserRegistration.java
@@ -1,22 +1,32 @@
 package com.UserRegistration;
 
+import java.util.function.Predicate;
+
 public class UserRegistration {
+    // Lambda Functions for Validation
+    private static final Predicate<String> firstNamePattern = name -> name.matches("^[A-Z][a-zA-Z]{2,}$");
+    private static final Predicate<String> lastNamePattern = name -> name.matches("^[A-Z][a-zA-Z]{2,}$");
+    private static final Predicate<String> emailPattern = email -> email.matches("^[a-zA-Z0-9]+([._%+-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9-]+(\\.[a-zA-Z]{2,}){1,2}$");
+    private static final Predicate<String> mobilePattern = mobile -> mobile.matches("^[0-9]{2} [0-9]{10}$");
+    private static final Predicate<String> passwordPattern = password -> password.matches("^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*(),.?\":{}|<>])[A-Za-z0-9!@#$%^&*(),.?\":{}|<>]{8,}$");
+
+
     public boolean validateFirstName(String firstName) throws InvalidUserDtlsException {
-        if(!firstName.matches("^[A-Z][a-zA-Z]{2,}$")){
+        if(!firstNamePattern.test(firstName)){
             throw new InvalidUserDtlsException("Invalid First Name");
         }
         return true;
     }
 
     public boolean validateLastName(String lastName) throws InvalidUserDtlsException {
-        if(!lastName.matches("^[A-Z][a-zA-Z]{2,}$")){
+        if(!lastNamePattern.test(lastName)){
             throw  new InvalidUserDtlsException("Invalid Last Name");
         }
         return true;
     }
 
     public boolean validateEmail(String email)throws InvalidUserDtlsException {
-        if(!email.matches("^[a-zA-Z0-9]+([._%+-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9-]+(\\.[a-zA-Z]{2,}){1,2}$")){
+        if(!emailPattern.test(email)){
             throw new InvalidUserDtlsException("Invalid Email!");
         }
         return true;
@@ -24,7 +34,7 @@ public class UserRegistration {
     }
 
     public boolean validateMobileNumber(String mobileNumber) throws InvalidUserDtlsException {
-        if(!mobileNumber.matches("^[0-9]{2} [0-9]{10}$")){
+        if(!mobilePattern.test(mobileNumber)){
             throw new InvalidUserDtlsException("Invalid Phone Number!");
         }
         return true;
@@ -47,18 +57,18 @@ public class UserRegistration {
         return true;
     }
     public boolean validatePasswordRule1(String password) {
-        return password.length() >= 8;
+        return passwordPattern.test(password);
     }
     public boolean validatePasswordRule2(String password) {
-        return password.matches(".*[A-Z].*");
+        return passwordPattern.test(password);
     }
 
     public boolean validatePasswordRule3(String password) {
-        return password.matches(".*[0-9].*");
+        return passwordPattern.test(password);
     }
 
     public boolean validatePasswordRule4(String password) {
-        return password.matches("^(?=.*[!@#$%^&*(),.?\":{}|<>])[a-zA-Z0-9!@#$%^&*(),.?\":{}|<>]{8,}$");
+        return passwordPattern.test(password);
     }
 
     public String validateUserDtls(String fName, String lName,String email,String pass,String mobNo) throws InvalidUserDtlsException {
